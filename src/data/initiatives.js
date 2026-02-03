afefc3c15212408ae85b023e4126b3017a490e09
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2026-02-03 17:22:17 +0530

    initiatives

--- src/data/initiatives.js
+++ src/data/initiatives.js
@@ -1,19 +1,81 @@
 export const initiatives = [
   {
     id: 1,
-    name: 'D-AMP Website',
-    photo: '../../Initiatives/Web.png',
-    link: 'https://mems-damp.netlify.app/',
-    description: 'Website for MEMS D-AMP',
-    subgroups: ['Web', 'Blogs and Info', 'Academic']
+    name: 'Sophomore 101',
+    photo: '../../Initiatives/Sophomore 101 - Presentation.png',
+    link: 'https://www.canva.com/design/DAGsNO7f-go/s_zMD3lkaPaKcNcbHSS0tQ/edit?utm_content=DAGsNO7f-go&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+    description: 'Sophomore 101, conducted in collaboration with UGAC, was designed to introduce students to the academics and opportunities in MEMS. The session featured 10 speakers from the DAMP team, who guided the students through the agenda outlined in the presentation, covering both core and non core opportunities',
+    subgroups: ['Events', 'Design', 'Academic', 'Info']
   },
   {
     id: 2,
-    name: 'D-AMP Resource Library',
+    name: 'Internship Preparation Session',
+    photo: '../../Initiatives/Internship Preparation Session.png',
+    link: 'https://www.canva.com/design/DAGtbG-nibM/1FCpEONuvoE8VGe2jlQVfg/edit?utm_content=DAGtbG-nibM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+    description: 'Conducted for thirdies with the goal of making them aware of all the different kind of domains and also what it needs to get an internship in each domain',
+    subgroups: ['Events', 'Design']
+  },
+  {
+    id: 3,
+    name: 'Sophie Research Booklet',
+    photo: '../../Initiatives/ResearchBooklet.png',
+    link: 'https://drive.google.com/file/d/1MPTMyl2c49mSrUaZAio7iYH8xRLDDfli/view',
+    description: 'A comprehensive guide for sophomores in the MEMS Department, this booklet includes SLP reviews, detailed overviews of sophomore courses, insights into academic and industry opportunities, pathways for higher studies in MEMS, and more.',
+    subgroups: ['Academic', 'Design']
+  },
+  {
+    id: 4,
+    name: 'MEMS D-AMP Trainings',
+    photo: '../../Initiatives/Trainings.png',
+    description: 'The MEMS DAMP training covered four key aspects: introduction to the mentorship role, handling sensitive issues, general discussions on exams, backlogs, and resources, and case-based roleplays. Mentors were guided on approaching delicate situations such as caste, gender, religion, disabilities, substance abuse, and academic integrity with empathy and responsibility. The sessions also included practical advice from senior mentors, tips for setting boundaries, and strategies to build trust and lasting bonds with mentees.',
+    subgroups: ['D-AMPCs']
+  },
+  {
+    id: 5,
+    name: 'Instagram Page',
+    photo: '../../Initiatives/Instagram.png',
+    link: 'https://www.instagram.com/mems_damp.iitb/',
+    description: 'We launched an Instagram page to share regular updates and information. As part of this, we introduced a series called Rulebook101, aimed at equipping students with essential academic knowledge. The page not only enhances the visibility of the MEMS DAMP team but also serves as an effective platform for outreach and promotion of upcoming events',
+    subgroups: ['Design', 'Info']
+  },
+  {
+    id: 6,
+    name: 'TSCs',
+    photo: '../../Initiatives/TSCs.png',
+    description: 'TSCs were conducted for all second-year courses and most third-year courses (except one), with the aim of providing last minute help to students before end semester and mid semester exams, covering core concepts and discussing PYQs as well, with strong participation across sessions. This initiative was carried out in collaboration with the SSS, where the DAMP team managed speaker onboarding and content review, while SSS handled outreach and logistics',
+    subgroups: ['Academic', 'Design']
+  },
+  {
+    id: 7,
+    name: 'D-AMP Blogs',
+    photo: '../../Initiatives/Web.png',
+    link: 'https://mems-damp.netlify.app/',
+    description: 'Everything MEMS D-AMP has to offer in one place!',
+    subgroups: ['Web']
+  },
+  {
+    id: 8,
+    name: 'Apping Guide',
+    photo: '../../Initiatives/AppingGuide.png',
+    link: 'https://drive.google.com/file/d/10WbcUr92Mh2T3A4vvGZKoeQiVMQ1SmF3/view',
+    description: 'The Apping guide was created with the goal of helping people who werent able to secure placements, interns through PT cell, as a way of helping them and letting them know all the other ways in which they can move forward in life',
+    subgroups: []
+  },
+  {
+    id: 9,
+    name: 'Resume Repository (Sophies)',
+    photo: '../../Initiatives/ResumeRepo.png',
+    link: 'https://drive.google.com/drive/folders/1BTTXoAVFofGPfI-KFvE_I2waLT0-RxTK?usp=drive_link',
+    description: 'A sophomore resume repository has been created, compiling the resumes of 2nd yr of all DAMP mentors to help reduce the pressure on 2nd-year students and guide them in preparing their own resumes',
+    subgroups: ['Info']
+  },
+  {
+    id: 10,
+    name: 'MEMS Student Resources',
     photo: '../../Initiatives/Resources.png',
     link: 'https://drive.google.com/drive/folders/1-2hrjpQsywBLLDJHudq7h68n2T2oQo1K',
-    description: 'Curated Resources for MEMS Students semester wise',
-    subgroups: ['Blogs and Info', 'Academic']
+    description: 'MEMS DAMP resources containing PYQs, lecture slides, assignments, tutorials for all MEMS courses from 1st Sem to 5th Sem and electives',
+    subgroups: ['Info', 'Academic']
   }
 ]
 
